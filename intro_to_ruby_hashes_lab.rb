90e496e9820cf4bc9900e6d58f467c20fabfbfc3
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 03:20:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,10 +45,10 @@ base_hash = {
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {mortgage_value: 100},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
+        :reading_railroad => {:mortgage_value => 100},
+        :pennsylvania_railroad => {:mortgage_value => 200},
+        :b_and_o_railroad => {:mortgage_value => 400},
+        :shortline_railroad => {:mortgage_value => 800}
       }
     }
   }
