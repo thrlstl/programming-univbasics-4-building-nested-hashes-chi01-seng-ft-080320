3e585f85d06692615f405410523f66cf2165e57e
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 03:13:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,11 @@ end
 
 def monopoly_with_second_tier
   base_hash = {
-    :railroads => {}
+    :railroads => {
+      :pieces => 4
+      :rent_in_dollars => {}
+      :names => {}
+    }
   }
 end
 
